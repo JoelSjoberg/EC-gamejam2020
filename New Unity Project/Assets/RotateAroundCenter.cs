c6b101285670596f089973d4bf6841dfb72116f3
Joel Sjöberg <joelsjoberg3@gmail.com>
2020-08-23 01:13:10 +0300

    Rotating asteroid, background image, goal sprite
    Added an asteroid which rotates around the center (found as child object in prefab!). Added sprites for the goal to emphasize objective. Added background to space.

---
+++ New Unity Project/Assets/RotateAroundCenter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundCenter : MonoBehaviour
+{
+    [SerializeField]
+    public Transform center;
+    public float degrees;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.RotateAround(center.position, Vector3.forward, degrees * Time.fixedDeltaTime);
+    }
+}
