ced2170dca5e0a7e0085afecf5a684997c02a5c3
Joel Sjöberg <joelsjoberg3@gmail.com>
2020-08-22 12:21:20 +0300

    Created base template for project

---
+++ New Unity Project/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    
+    Rigidbody2D rb;
+    Vector2 acceleration, velocity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
