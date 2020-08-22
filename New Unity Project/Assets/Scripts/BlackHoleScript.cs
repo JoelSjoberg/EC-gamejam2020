27670540ef3417c2565fd86dca00d05fbc1defa1
Patric Gustafsson <patricjgustafsson@outlook.com>
2020-08-22 19:49:38 +0300

    Add blackhole

---
+++ New Unity Project/Assets/Scripts/BlackHoleScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Black holes that currently affect anyhting inside its radius. 
+/// </summary>
+public class BlackHoleScript : MonoBehaviour
+{
+    float gconst = 6.674e-11f; // From wikipedia
+    public float mass; // This mass is only used for calculation of gravitational acceleration and not actually in physics
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mass = 1e10f; // With this you can still escape and "wrap around" the black hole
+    }
+
+    void OnTriggerStay2D(Collider2D collider2D) {
+        Vector2 dist = transform.position - collider2D.transform.position;
+
+        // object is "consumed" by the hole
+        if (dist.magnitude < 0.01) { 
+            Destroy(collider2D.gameObject);
+            return;
+        }
+
+        // Calculate gravitional acceleration. The mass of the black hole should be massive so that 
+        // it can affect any other object. 
+        float r = dist.magnitude;
+        float f = gconst * (mass * collider2D.attachedRigidbody.mass) / r;
+
+        collider2D.attachedRigidbody.velocity += dist.normalized * f * Time.deltaTime;
+    }
+}
--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -61,8 +61,6 @@ public class PlayerController : MonoBehaviour
         }
 
         ConsumeFuelIfNeeded();
-
-        Debug.Log(fuel);
     }
 
     void FixedUpdate()
