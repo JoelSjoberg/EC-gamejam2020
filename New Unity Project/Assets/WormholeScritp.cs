e18b23b87d6eec12a5ffa4a4bd3d3bf28f199d68
Patric Gustafsson <patricjgustafsson@outlook.com>
2020-08-22 18:07:23 +0300

    Add "OneWayWormhole"
    One way wormhole -> can only teleport one way

---
+++ New Unity Project/Assets/WormholeScritp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormholeScritp : MonoBehaviour
+{
+    public Vector2 end;
+    GameObject wormholeEnd; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wormholeEnd = transform.GetChild(0).gameObject;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.tag == "Player") {
+            collider.transform.position = wormholeEnd.transform.position;
+        }
+    }
+}
