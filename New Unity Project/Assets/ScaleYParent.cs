5222ab0a5ef7e2d318825fc364cc744f197f33e1
Joel Sjöberg <joelsjoberg3@gmail.com>
2020-08-24 01:00:08 +0300

    ForceField particles

---
+++ New Unity Project/Assets/ScaleYParent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleYParent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = transform.parent.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
