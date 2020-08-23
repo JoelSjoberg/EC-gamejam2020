17e72128acb04feaa9aef7e9a353f4df385f9b91
Patric Gustafsson <patricjgustafsson@outlook.com>
2020-08-23 22:20:45 +0300

    GUI text fuel and health

---
+++ New Unity Project/Assets/HealthScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour
+{
+    Text healthString;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthString = GetComponent<Text>();
+        healthString.text = GameStats.hp.ToString();
+    }
+
+    // This is not nice. Now this is updated many times even though not changing. 
+    public void Update()
+    {
+        healthString.text = GameStats.hp.ToString(); // ugly
+    }
+}
