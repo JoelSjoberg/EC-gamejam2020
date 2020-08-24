72688c83edc3498a935c3f99a4f3e10e11e6fcc8
Joel Sjöberg <joelsjoberg3@gmail.com>
2020-08-24 17:38:58 +0300

    Music, player gain health on goal reached

--- New Unity Project/Assets/Scripts/Goal.cs
+++ New Unity Project/Assets/Scripts/Goal.cs
@@ -10,6 +10,7 @@ public class Goal : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.tag == "Player") 
         {
+            GameStats.hp += 1;
             SceneManager.LoadScene(nextLevel);
         }
     }
---
+++ New Unity Project/Assets/dontDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dontDestroy : MonoBehaviour
+{
+    private static bool exists = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (exists) 
+        {
+            Destroy(gameObject);
+        }
+        else 
+        {
+            exists = true;
+            DontDestroyOnLoad(this.gameObject);
+        } 
+    }
+}
